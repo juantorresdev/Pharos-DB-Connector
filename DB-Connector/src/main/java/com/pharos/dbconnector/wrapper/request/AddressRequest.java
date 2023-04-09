5527eb1d99f7db6be353ec92e0ceb48b0307670f
juantorresdev <juan.torres.dev@gmail.com>
2023-04-08 23:27:34 -0600

    33 Generated properties for request wrappers

--- DB-Connector/src/main/java/com/pharos/dbconnector/wrapper/request/AddressRequest.java
+++ DB-Connector/src/main/java/com/pharos/dbconnector/wrapper/request/AddressRequest.java
@@ -1,4 +1,18 @@
 package com.pharos.dbconnector.wrapper.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AddressRequest {
+    @JsonProperty("pharosid")
+    public String pharosId;
+    @JsonProperty("street")
+    public String street;
+    @JsonProperty("city")
+    public String city;
+    @JsonProperty("province")
+    public String province;
+    @JsonProperty("postcode")
+    public String postcode;
+    @JsonProperty("country")
+    public String country;
 }
--- DB-Connector/src/main/java/com/pharos/dbconnector/wrapper/request/GenderRequest.java
+++ DB-Connector/src/main/java/com/pharos/dbconnector/wrapper/request/GenderRequest.java
@@ -1,4 +1,12 @@
 package com.pharos.dbconnector.wrapper.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GenderRequest {
+    @JsonProperty("pharosid")
+    public String pharosId;
+    @JsonProperty("gender")
+    public String gender;
+    @JsonProperty("genderdesc")
+    public String genderDescription;
 }
