850edd954808b32bda0f4487bdc60f10dbe31885
Juan Torres <57916724+juantorresdev@users.noreply.github.com>
2023-04-17 05:18:57 +0000

    41 Added Swagger documentation for main class

--- DB-Connector/src/main/java/com/pharos/dbconnector/DbConnectorApplication.java
+++ DB-Connector/src/main/java/com/pharos/dbconnector/DbConnectorApplication.java
@@ -1,8 +1,33 @@
 package com.pharos.dbconnector;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Juan Torres",
+						email = "juan.torres.devt@gmail.com",
+						url = "Pending"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "Pending"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation",
+				url = "Pending"
+		)
+)
 @SpringBootApplication
 public class DbConnectorApplication {
 
